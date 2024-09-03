3b1ac4b69dcb12396f07a0f36fce9717651f5ea3
jjm159 <wjdwoaud15@gmail.com>
2024-09-03 16:32:29 +0900

    init: 프로젝트 구조 설정

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from controller import main, login, user_profile
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    return render_template('index.html')
+app.register_blueprint(main.bp)
+app.register_blueprint(login.bp)
+app.register_blueprint(user_profile.bp)
+
+# TODO: sign_up
 
 if __name__ == '__main__':
     # debug를 True로 세팅하면, 해당 서버 세팅 후에 코드가 바뀌어도 문제없이 실행됨. 
     app.run(host='127.0.0.1', port=8000, debug = True)
-
-
---
+++ controller/main.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__)
+
+@bp.route("/main")
+def main():
+    return render_template('main.html')
