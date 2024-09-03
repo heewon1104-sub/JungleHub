3740bab1e80183330c5e7d04ee600bc0ca0fcb73
김희원 <julian3306@naver.com>
2024-09-03 17:56:35 +0900

    feature-puzzle: 메인페이지 퍼즐 컴포넌트 구현

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from controller import main, login, user_profile
-from module.scheduler import Scheduler
-import xml.etree.ElementTree as elementTree
-
-from repository.config import RepositoryConfig
 
 app = Flask(__name__)
 
@@ -12,7 +8,6 @@ app.register_blueprint(main.bp)
 app.register_blueprint(login.bp)
 app.register_blueprint(user_profile.bp)
 # TODO: sign_up
-
 
 if __name__ == '__main__':
     # debug를 True로 세팅하면, 해당 서버 세팅 후에 코드가 바뀌어도 문제없이 실행됨. 
